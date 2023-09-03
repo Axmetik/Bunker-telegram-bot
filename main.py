7fcd7309d32d7f783d08665830f17ca117eca57c
Axmetik <93944284+Axmetik@users.noreply.github.com>
2023-09-03 14:17:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import adding
 
 
 #пдіключення бота через унікальний токен
-bot = telebot.TeleBot(***) //unique token
+bot = telebot.TeleBot(***) #unique token
 
 #Хендлер для прийому команди /start
 @bot.message_handler(commands=['start'])
